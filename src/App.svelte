e94995ce634e3a7b5fa34bdd9ddc2d4ac6ce932a
WeichenTie <75655299+WeichenTie@users.noreply.github.com>
2023-12-15 14:33:35 +1100

    added tailwind autosort

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+  import "./app.css";
+  import * as THREE from "three";
+  import * as SC from "svelte-cubed";
 </script>
 
-<nav></nav>
-<main>
-  <h1>Hi I'm Weichen and I like to build cool stuff</h1>
+<main class="h-screen w-full text-yellow-300">
+  <div class="h-full w-full bg-[url('./assets/background.png')] bg-cover">
+    <div class="absolute -z-50">
+      <!-- <SC.Canvas>
+        <SC.Mesh geometry={new THREE.BoxGeometry()} />
+        <SC.PerspectiveCamera position={[1, 1, 3]} />
+      </SC.Canvas> -->
+    </div>
+    <h1>Hi I'm Weichen and I like to build cool stuff</h1>
+  </div>
 </main>
